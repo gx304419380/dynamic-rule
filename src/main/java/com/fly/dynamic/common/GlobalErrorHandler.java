7a11ebc68c3f189d3ba418f6f07c270a4cd99cb9
guoxiang <18841845137@163.com>
2021-07-30 16:37:50 +0800

    [UPD]change the static files

--- src/main/java/com/fly/dynamic/common/GlobalErrorHandler.java
+++ src/main/java/com/fly/dynamic/common/GlobalErrorHandler.java
@@ -2,8 +2,6 @@ package com.fly.dynamic.common;
 
 import com.fly.dynamic.entity.RuleResult;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @since 2021/7/26
  */
 @RestControllerAdvice(basePackages = "com.fly.dynamic.controller")
-@Order(Ordered.HIGHEST_PRECEDENCE)
 @Slf4j
 public class GlobalErrorHandler {
 
